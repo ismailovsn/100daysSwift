b990e8a17811a24ff07a7fa7ff513e6dc4908968
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-10 07:17:04 +0900

    feat: Cupcake Corner project. create the data model and new view

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Саид-Насир Исмаилов on 2023/05/09.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
