60171c58c9242abf777a1d102f99bdfcf27ab65f
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-10 11:27:36 +0900

    feat: fix bug with white spacing in the adress

--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -34,9 +34,13 @@ class Order: ObservableObject, Codable {
     @Published var zip = ""
     
     var hasValidAddres: Bool {
-        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+        if name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
+            streetAddress.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
+            city.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
+            zip.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
             return false
         }
+
         
         return true
     }
