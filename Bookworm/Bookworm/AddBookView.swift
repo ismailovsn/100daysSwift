577ed0a48580ed3daafec4a3239e7c86c30ed09c
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-11 20:56:30 +0900

    feat: create a add book button in conent view and make save button dismiss the view

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AddBookView: View {
     @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
     
     @State private var title = ""
     @State private var author = ""
@@ -54,6 +55,7 @@ struct AddBookView: View {
                             newBook.review = review
                             
                             try? moc.save()
+                            dismiss()
                         }
                     }
                 }
