78b0a2ee2b1a70980dc1580a555184fbbc20ac10
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-12 09:51:07 +0900

    feat: create a add book button in conent view and make save button dismiss the view

---
+++ Bookworm/Bookworm/EmojiRatingView.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Саид-Насир Исмаилов on 2023/05/12.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("🥴")
+        case 2:
+            return Text("😕")
+        case 3:
+            return Text("🙂")
+        case 4:
+            return Text("😊")
+        default:
+            return Text("🤩")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
