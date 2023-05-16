fa2cd56533f96d782cca002acc0fbc8b1335174f
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-16 17:13:17 +0900

    feat: start Bucketlist project

---
+++ Bucketlist/Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Саид-Насир Исмаилов on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
