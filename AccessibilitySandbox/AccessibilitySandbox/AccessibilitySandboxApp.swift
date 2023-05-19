ad65614c681ed66c9ff88b933d42867c3f4ff080
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-20 06:35:57 +0900

    feat: start Accessibility

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Саид-Насир Исмаилов on 2023/05/20.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
