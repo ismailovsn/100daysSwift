02a6d1df5a4a1173b15d9610fabbcec408b48e35
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-20 11:24:31 +0900

    feat: start HotProspects project

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Саид-Насир Исмаилов on 2023/05/20.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
