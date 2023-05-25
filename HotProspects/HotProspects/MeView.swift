0fbbf3b35ac320b94f1b7384114a9a83f8c6f9a0
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-25 14:06:07 +0900

    feat: add a context menu to an image

--- HotProspects/HotProspects/MeView.swift
+++ HotProspects/HotProspects/MeView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var emailAddress = "you@yoursite.com"
+    @State private var qrCode = UIImage()
     
     let context  = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -27,21 +28,29 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCODE(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .resizable()
                     .interpolation(.none)
                     .scaledToFit()
                     .frame(width: 200, height: 200)
                     .contextMenu {
                         Button {
-                            //save
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
                         } label: {
                             Label("Save to photos", systemImage: "square.and.arrow.down")
                         }
                     }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name) { _ in updateCode() }
+            .onChange(of: emailAddress) { _ in updateCode() }
         }
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCODE(from: "\(name)\n\(emailAddress)")
     }
     
     func generateQRCODE(from string: String) -> UIImage {
@@ -50,6 +59,7 @@ struct MeView: View {
         if let outputImage = filter.outputImage {
             if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
                 return UIImage(cgImage: cgimg)
+                
             }
         }
         
