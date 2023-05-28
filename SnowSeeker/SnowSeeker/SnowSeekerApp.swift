27b074fd7f5f48be46642eabe6a72196677c20c4
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-29 06:48:11 +0900

    feat: start SnowSeeker project

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Саид-Насир Исмаилов on 2023/05/29.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
